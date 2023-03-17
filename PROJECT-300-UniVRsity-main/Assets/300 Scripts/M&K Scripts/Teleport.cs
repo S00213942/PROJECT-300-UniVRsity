d2193d1b0f7bbb830de94b6b288683343b236d31
EmmetOneT <emmetcarroll94@gmail.com>
2023-03-17 03:43:39 +0000

    Safety Commit
    Please don't crash again

--- PROJECT-300-UniVRsity-main/Assets/300 Scripts/M&K Scripts/Teleport.cs
+++ PROJECT-300-UniVRsity-main/Assets/300 Scripts/M&K Scripts/Teleport.cs
@@ -6,15 +6,46 @@ using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
-    public Transform teleportDestination;
+    public Transform RC,CR, CA, AC, CD, DC;
     void OnTriggerEnter(Collider teleport)
     {
 
         if (teleport.gameObject.tag == "Teleport")
         {
 
-            transform.position = teleportDestination.position;
+            transform.position = RC.position;
             
         }
+        if (teleport.gameObject.tag == "Teleport1")
+        {
+
+            transform.position = CR.position;
+
+        }
+        if (teleport.gameObject.tag == "Teleport2")
+        {
+
+            transform.position = CA.position;
+
+        }
+        if (teleport.gameObject.tag == "Teleport3")
+        {
+
+            transform.position = AC.position;
+
+        }
+        if (teleport.gameObject.tag == "Teleport4")
+        {
+
+            transform.position = CD.position;
+
+        }
+        if (teleport.gameObject.tag == "Teleport5")
+        {
+
+            transform.position = DC.position;
+
+        }
+
     }
 }
